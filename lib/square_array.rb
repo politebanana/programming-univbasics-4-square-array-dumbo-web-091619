3a59f0e6c34b7f20ddfb7117e450912162bd9828
Christian Zambrana <chrislajas@gmail.com>
2019-07-11 11:31:31 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -3,7 +3,7 @@ def square_array(array)
   new_array = []
   count = 0
   while count < array.size do
-    squared << (array[count] ** 2)
+    new_array << (array[count] ** 2)
     count = count + 1
   end
   squared
